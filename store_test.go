4ef9ac5700ef8ce429f1a84aa7fcbcf908e9b8d5
Muhammad Ismail Zam Zam <mismailzz75@gmail.com>
2025-12-23 15:09:02 +0000

    made test to run 50 times, just to see everything works fine

--- store_test.go
+++ store_test.go
@@ -5,44 +5,56 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"strconv"
 )
 
 func TestStore(t *testing.T){
 	
-	// 1. Initialize Store
-	opts := StoreOpts {
-		PathTransformFunc: CASPathTransformFunc,
-		RootDir: RootDirPathname,
+	for i :=0; i < 50; i++ { // running the same test 50 times for different file
+
+		// 1. Initialize Store
+		opts := StoreOpts {
+			PathTransformFunc: CASPathTransformFunc,
+			RootDir: RootDirPathname,
+		}
+		s := NewStore(opts)
+
+		// 2. Define a file 
+		key := "examplefile" + "_" + strconv.Itoa(i)
+		data := []byte("hello world!")
+
+		// 3. Write to a file 
+		if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
+			t.Errorf("writeStream %s\n", err)
+		}
+
+		// 4. Read to a file 
+		r, err := s.readStream(key)
+		if err != nil { 
+			t.Errorf("readStream %s\n", err)
+		}
+
+		b, _ := ioutil.ReadAll(r)
+		fmt.Println(string(b))
+
+		if string(b) != string(data){
+			t.Errorf("want %s have %s", data, b)
+		}
+
+		// 5. Delete file 
+
+		if err = s.Delete(key); err != nil {
+			t.Errorf("Error deleting file %s\n", err)
+		}
+
+		// 6. Just Verify if the file exist or not 
+		pathKey := s.PathTransformFunc(key)
+		filenameFullPathWithRoot := pathKey.FullPath(s.RootDir)
+		if fileExist(filenameFullPathWithRoot){
+			t.Errorf("File %s should have to be deleted, but it exist", filenameFullPathWithRoot)
+		}
 	}
-	s := NewStore(opts)
-
-	// 2. Define a file 
-	key := "exampleFile"
-	data := []byte("hello world!")
-
-	// 3. Write to a file 
-	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
-		t.Errorf("writeStream %s\n", err)
-	}
-
-	// 4. Read to a file 
-	r, err := s.readStream(key)
-	if err != nil { 
-		t.Errorf("readStream %s\n", err)
-	}
-
-	b, _ := ioutil.ReadAll(r)
-	fmt.Println(string(b))
-
-	if string(b) != string(data){
-		t.Errorf("want %s have %s", data, b)
-	}
-
-	// 5. Delete file 
-
-	if err = s.Delete(key); err != nil {
-		t.Errorf("Error deleting file %s\n", err)
-	}
+	
 }
 
 // func TestCASPathTransformFunc(t *testing.T) {
