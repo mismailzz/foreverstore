da884db5aab86f2038e527b567c93939dd6b0e00
Muhammad Ismail Zam Zam Butt <mismailzz75@gmail.com>
2025-12-28 18:04:49 +0100

    refactor main

--- main.go
+++ main.go
@@ -2,37 +2,41 @@ package main
 
 import (
 	"log"
-	"time"
-
 	"github.com/mismailzz/foreverstore/p2p"
 )
 
 func main() {
 
+	s1 := makeServer(":3000", "")
+	s2 := makeServer(":4000", ":3000")
+
+	go func(){
+		log.Fatal(s1.Start())
+	}()
+
+	s2.Start()
+
+}
+
+func makeServer(listenAddr string, peerNodes ...string) *FileServer {
+
+	// Initialize TCP transport
 	transportOpts := p2p.TCPTransportOpts{
-		ListenAddress: ":3000",
+		ListenAddress: listenAddr,
 		Shakehand:     p2p.NoHandShakeFunc,
 		Decoder:       &p2p.DefaultDecoder{},
 		// TODO: OnPeer func
 	}
 	tcpTransport := p2p.NewTCPTransport(transportOpts)
 
+	// File server options configuration
 	fileServerOpts := FileServerOpts{
-		StorageRootDir:    "3000_network", // to differentiate and storage in the future client files in different port
+		StorageRootDir:    listenAddr + "_network",
 		PathTransformFunc: CASPathTransformFunc,
 		Transport:         tcpTransport,
-		BootstrapNodes:    []string{":3001", ":3002", ":3000"}, // assuming we have other nodes running on these ports
+		BootstrapNodes:    peerNodes, 
 	}
-
 	server := NewFileServer(fileServerOpts)
 
-	go func() { // stop server after 3 secoond from starting server
-		time.Sleep(time.Second * 3) //after 3 sec
-		server.Stop()
-	}()
-
-	if err := server.Start(); err != nil {
-		log.Fatalf("Error occurred during starting the server: %s\n", err)
-	}
-
+	return server
 }
