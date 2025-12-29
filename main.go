944321e7eedb33818623424a32ee4f0099142f7a
Muhammad Ismail Zam Zam Butt <mismailzz75@gmail.com>
2025-12-29 18:57:40 +0100

    refactor

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"time"
 
@@ -19,12 +20,12 @@ func main() {
 	time.Sleep(1 * time.Second) // provide some time for server 1 to start
 
 	go s2.Start()
-	// time.Sleep(1 * time.Second) // provide some time for server 2 to start and connect to server 1
+	time.Sleep(1 * time.Second) // provide some time for server 2 to start and connect to server 1
 
-	// data := bytes.NewReader([]byte("my big data file"))
-	// if err := s2.StoreData("file1.txt", data); err != nil {
-	// 	log.Fatal(err)
-	// }
+	data := bytes.NewReader([]byte("my big data file"))
+	if err := s2.StoreData("file1.txt", data); err != nil {
+		log.Fatal(err)
+	}
 
 	select {} // block foreve
 
